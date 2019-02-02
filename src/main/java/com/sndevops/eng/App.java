4cff9f94aa5ac5c0c8e52a76d4177e84b726567d
rohan.rasane <rohan.rasane@servicenow.com>
2019-02-01 20:16:26 -0800

    1st in

---
+++ src/main/java/com/sndevops/eng/App.java
@@ -0,0 +1,13 @@
+package com.sndevops.eng;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello devops!" );
+    }
+}
