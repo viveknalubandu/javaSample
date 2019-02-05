524a0f58b69a7e6d2e7024f5b147818f08a58665
viveknalubandu <vivek091@gmail.com>
2019-02-05 15:45:58 -0800

    Update AppTest.java

--- src/test/java/com/sndevops/eng/AppTest.java
+++ src/test/java/com/sndevops/eng/AppTest.java
@@ -7,9 +7,12 @@ import org.junit.Test;
 public class AppTest {
 
     private String INPUT = "123456";
+    private String INPUT1 = "1234567";
+    
     @Test
     public void testLength() {
         // Add few more details. More details to be added 
         Assert.assertEquals(INPUT,"123456");
+        Assert.assertEquals(INPUT1,"1234567");
     }
 }
