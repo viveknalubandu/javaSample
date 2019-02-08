9022f2f874afead38e85d06ff9f66f88c9250bd9
viveknalubandu <vivek091@gmail.com>
2019-02-08 14:41:01 -0800

    Update AppTest.java

--- src/test/java/com/sndevops/eng/AppTest.java
+++ src/test/java/com/sndevops/eng/AppTest.java
@@ -7,13 +7,14 @@ import org.junit.Test;
 public class AppTest {
 
     private String INPUT = "123456";
-//     private String INPUT1 = "1234567";
-//     private String INPUT2 = "12345678";
+    private String INPUT1 = "1234567";
+    private String INPUT2 = "12345678";
     private boolean isPassed = true;
     @Test
     public void testLength() {
         // Add few more details. More details to be added 
         Assert.assertEquals(INPUT,"123456");
+        Assert.assertEquals(INPUT2,"12345678");
     }
     
     @Test
