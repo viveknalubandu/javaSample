9065d788fb9288b6ec091ff6025fcf14cdc69d26
viveknalubandu <vivek091@gmail.com>
2019-02-08 14:19:48 -0800

    Add line
    Add line

--- src/main/java/com/sndevops/eng/App.java
+++ src/main/java/com/sndevops/eng/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello devops. How are you" );
-        System.out.println( "Hello . Give me an apple" );
+        System.out.println( "Hello . Give me an pineapple" );
     }
 }
