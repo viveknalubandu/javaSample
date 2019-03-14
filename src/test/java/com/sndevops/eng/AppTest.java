6577bdf95d69e31ba4cf4d5757cdeb632929f076
viveknalubandu <vivek091@gmail.com>
2019-03-14 14:10:31 -0700

    CM3

--- src/test/java/com/sndevops/eng/AppTest.java
+++ src/test/java/com/sndevops/eng/AppTest.java
@@ -8,14 +8,14 @@ public class AppTest {
 
     private String INPUT = "123456";
     private String INPUT1 = "1234567";
-    private String INPUT2 = "1113";
+    private String INPUT2 = "1115";
     private boolean isPassed = true;
     // Add more tests
     @Test
     public void testLength() {
         // Add few more details. More details to be added 
         Assert.assertEquals(INPUT,"123456");
-        Assert.assertEquals(INPUT2,"1113");
+        Assert.assertEquals(INPUT2,"1115");
     }
     
     @Test
