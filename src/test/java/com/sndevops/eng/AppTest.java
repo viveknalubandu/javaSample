615143a191797b6ef03ed44ffd515a7e46193db8
viveknalubandu <vivek091@gmail.com>
2019-07-02 12:07:58 -0700

    TM

--- src/test/java/com/sndevops/eng/AppTest.java
+++ src/test/java/com/sndevops/eng/AppTest.java
@@ -5,7 +5,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class AppTest {
-    private String INPUT = "1";
+    private String INPUT = "2";
     private String INPUT1 = "1234567";
     private String INPUT2 = "115";
     private boolean isPassed = true;
@@ -13,7 +13,7 @@ public class AppTest {
     @Test
     public void testLength() {
         // Test with new Dev build 1
-        Assert.assertEquals(INPUT,"1");
+        Assert.assertEquals(INPUT,"2");
         Assert.assertEquals(INPUT2,"115");
     }
     
